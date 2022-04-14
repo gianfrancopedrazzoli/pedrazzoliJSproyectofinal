f5ff8b93b10e445d36dcba4a71801485c58301d0
gianfrancopedrazzoli <regtestinblack@gmail.com>
2022-04-14 23:01:46 +0200

    :D

--- javascript.js
+++ javascript.js
@@ -1,79 +1,56 @@
-/* eslint-disable linebreak-style */
-/* eslint-disable indent */
-/* eslint-disable no-restricted-globals */
-/* eslint-disable no-console */
-/* eslint-disable linebreak-style */
-/* eslint-disable no-alert */
-const validar = confirm('desea crear una cuenta?');
-let nombreUsuario;
-let passwordUsuario;
-let listasUsuarios = [];
-
-function crearUsuario() {
-  while (nombreUsuario === null || nombreUsuario.trim().length === 0) {
-    alert('ese usuario no es valido');
-    nombreUsuario = (prompt('ingresa un usuario') || '').toLowerCase();
-  }
-  alert('usuario registrado');
-  return true;
-}
-
-function crearPassword() {
-  while (passwordUsuario.trim().length <= 7) {
-    alert('contraseña no valida. Recuerda ingresar al menos 8 carcteres, no se admiten espacios.');
-    passwordUsuario = (prompt('ingresar contraseña:') || '');
-  }
-  alert('contraseña registrada');
-  console.log(`hola ${nombreUsuario}! \ntu contraseña es: ${passwordUsuario}`);
-  return true;
-}
-
-// function cancelarInicioSesion() {
-// }
-
-function validarIngresoSitio() {
-  const CONFIRMACION_USER = confirm('¿quiere ingresar a al sitio?');
-  if (CONFIRMACION_USER === false) {
-    return false;
-    // return document.write('no accediste');
-    // cancelarInicioSesion();
-    // return validarIngresoSitio();
-  }
-  return true;
-}
-
-function ingresarUsuario() {
-  const nombreUsuarioCheck = (prompt('ingrese su usuario:') || '');
-  if (nombreUsuarioCheck === nombreUsuario) {
-    let passwordUser = (prompt('ingresa su contraseña:') || '');
-    if (passwordUser === passwordUsuario) {
-      document.write(`Bienvenido ${nombreUsuario}`);
-    } else if (passwordUser !== passwordUsuario) {
-      alert('la contraseña ingresada no es la correcta');
-      while (passwordUser !== passwordUsuario) {
-        passwordUser = (prompt('ingresa su contraseña:') || '');
-        document.write(`Bienvenido ${nombreUsuario}`);
-      }
+const LISTADO_PRODUCTOS = [];
+class Producto {
+    constructor(nombre, categoria, precio, stock) {
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.precio = precio;
+        this.stock = stock;
     }
-  } else {
-    alert('usuario incorrecto');
-    ingresarUsuario();
-  }
+
+    descuento() {
+        this.precio *= 0.9;
+        return this.precio;
+    }
+
+    cambioNombre() {
+        let nombreProd = prompt('ingrese un nuevo nombre');
+        this.nombre = nombreProd;
+        return this.nombre;
+    }
+
+    agregarStock() {
+        let cantidadStock = Number(prompt('ingrese cantidad'));
+        while (isNaN(cantidadStock || null)) {
+            cantidadStock = Number(prompt('ingrese cantidad'));
+        }
+        this.stock += cantidadStock;
+    }
+
+    venta() {
+        let ventaProducto = Number(prompt('ingrese cantidad a vender'));
+        if (ventaProducto > this.stock || this.stock === 0) {
+            alert(`ùsted posee ${this.stock} unidades del producto`);
+            alert('no hay unidades sufucientes para la venta');
+            let confirmAddStock = confirm('desea agregar stock?');
+            if (confirmAddStock === true) {
+                return this.agregarStock();
+            } else {
+                alert('no agrego stock');
+            }
+        }
+        if (ventaProducto <= this.stock) {
+            this.stock -= ventaProducto;
+            return this.stock;
+        }
+    }
 }
 
-function crearCuenta() {
-  crearUsuario();
-  crearPassword();
-  validarIngresoSitio();
-  ingresarUsuario();
-}
+function agregarProducto() {
+    let nombreProducto = prompt('ingrese el nombre del producto');
+    let categoriaProducto = prompt('ingrese la categoria del producto');
+    let precioProducto = Number(prompt('ingrese el precio'));
+    let stockProducto = Number(prompt('ingrese el stock'));
 
-if (validar === true) {
-  nombreUsuario = (prompt('crea un usuario') || '').toLowerCase();
-  passwordUsuario = (prompt('ingresa una contraseña de al menos 8 caracteres (sin espacios)') || '');
-  crearCuenta();
-} else {
-  alert('no ingresaste al sitio');
+    const PROD = new Producto(nombreProducto, categoriaProducto, precioProducto, stockProducto);
+    LISTADO_PRODUCTOS.push(PROD);
 }
-
-console.log(listasUsuarios);
